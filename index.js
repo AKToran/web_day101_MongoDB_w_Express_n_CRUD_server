6672980d5feee73184f3845c601be4e018b9a881
AKToran <aktoran60@gmail.com>
2025-05-20 11:38:03 +0600

    connected mongodb server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +7,31 @@ const port = process.env.PORT || 3000;
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://toranbd:${process.env.db_password}@cluster0.cjjjauk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+    
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res)=>{
